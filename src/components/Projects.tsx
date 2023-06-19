1f6aee4d9cbedea42dfdf2626e2e17a6a172fc04
pathran-phantom1823 <parick.pathran23@gmail.com>
2023-06-19 10:18:43 +0800

    initial projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,17 @@ import React from 'react'
 function Projects() {
   return (
     <div>
-        <header>PROJECT</header>
+        <header className='text-[40px] font-bold flex flex-row justify-center items-center'>
+          PROJECTS
+        </header>
+        <section className='projectList'>
+          <div className='flex justify-between items-center'>
+            <div>
+              <p>Traceag</p>
+            </div>
+            
+          </div>
+        </section>
     </div>
   )
 }
